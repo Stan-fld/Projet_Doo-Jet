3913f1277431197e9fba60374fb53db59e2a0bc1
Stan-fld <stanislasfoillard@live.fr>
2020-07-31 11:52:43 +0200

    Modif header

--- mvc/view/header.php
+++ mvc/view/header.php
@@ -56,9 +56,17 @@
                     <a href="/">
                         <i class="fas fa-tachometer-alt"></i>Accueil</a>
                 </li>
-                <li>
-                    <a href="/employe">
+                <li <?= $nomPage[0] == 'employe' || $nomPage[0] == 'createemploye'? ' class="active"' : '' ?>>
+                    <a class="js-arrow" href="#" >
                         <i class="fas fa-user"></i>Employé</a>
+                    <ul class="list-unstyled navbar__sub-list js-sub-list">
+                        <li <?= $nomPage[0] == 'employe' ? ' class="active"' : '' ?>>
+                            <a href="/employe">Employé</a>
+                        </li>
+                        <li <?= $nomPage[0] == 'createemploye' ? ' class="active"' : '' ?>>
+                            <a href="/createemploye">Ajouter employé</a>
+                        </li>
+                    </ul>
                 </li>
                 <li>
                     <a href="/createclient">
@@ -91,45 +99,6 @@
                         </li>
                     </ul>
                 </li>
-                <li class="has-sub">
-                    <a class="js-arrow" href="#">
-                        <i class="fas fa-desktop"></i>UI Elements</a>
-                    <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
-                        <li>
-                            <a href="button.html">Button</a>
-                        </li>
-                        <li>
-                            <a href="badge.html">Badges</a>
-                        </li>
-                        <li>
-                            <a href="tab.html">Tabs</a>
-                        </li>
-                        <li>
-                            <a href="card.html">Cards</a>
-                        </li>
-                        <li>
-                            <a href="alert.html">Alerts</a>
-                        </li>
-                        <li>
-                            <a href="progress-bar.html">Progress Bars</a>
-                        </li>
-                        <li>
-                            <a href="modal.html">Modals</a>
-                        </li>
-                        <li>
-                            <a href="switch.html">Switchs</a>
-                        </li>
-                        <li>
-                            <a href="grid.html">Grids</a>
-                        </li>
-                        <li>
-                            <a href="fontawesome.html">Fontawesome Icon</a>
-                        </li>
-                        <li>
-                            <a href="typo.html">Typography</a>
-                        </li>
-                    </ul>
-                </li>
             </ul>
         </div>
     </nav>
@@ -150,9 +119,17 @@
                     <a href="/">
                         <i class="fas fa-tachometer-alt"></i>Accueil</a>
                 </li>
-                <li<?= $nomPage[0] == 'employe' ? ' class="active"' : '' ?>>
-                    <a href="/employe">
+                <li <?= $nomPage[0] == 'employe' || $nomPage[0] == 'createemploye'? ' class="active"' : '' ?>>
+                    <a class="js-arrow" href="#" >
                         <i class="fas fa-user"></i>Employé</a>
+                    <ul class="list-unstyled navbar__sub-list js-sub-list">
+                        <li <?= $nomPage[0] == 'employe' ? ' class="active"' : '' ?>>
+                            <a href="/employe">Employé</a>
+                        </li>
+                        <li <?= $nomPage[0] == 'createemploye' ? ' class="active"' : '' ?>>
+                            <a href="/createemploye">Ajouter employé</a>
+                        </li>
+                    </ul>
                 </li>
                 <li <?= $nomPage[0] == 'createclient' ? ' class="active"' : '' ?>>
                     <a href="createclient">
@@ -182,45 +159,6 @@
                         </li>
                         <li>
                             <a href="forget-pass.html">Forget Password</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="has-sub">
-                    <a class="js-arrow" href="#">
-                        <i class="fas fa-desktop"></i>UI Elements</a>
-                    <ul class="list-unstyled navbar__sub-list js-sub-list">
-                        <li>
-                            <a href="button.html">Button</a>
-                        </li>
-                        <li>
-                            <a href="badge.html">Badges</a>
-                        </li>
-                        <li>
-                            <a href="tab.html">Tabs</a>
-                        </li>
-                        <li>
-                            <a href="card.html">Cards</a>
-                        </li>
-                        <li>
-                            <a href="alert.html">Alerts</a>
-                        </li>
-                        <li>
-                            <a href="progress-bar.html">Progress Bars</a>
-                        </li>
-                        <li>
-                            <a href="modal.html">Modals</a>
-                        </li>
-                        <li>
-                            <a href="switch.html">Switchs</a>
-                        </li>
-                        <li>
-                            <a href="grid.html">Grids</a>
-                        </li>
-                        <li>
-                            <a href="fontawesome.html">Fontawesome Icon</a>
-                        </li>
-                        <li>
-                            <a href="typo.html">Typography</a>
                         </li>
                     </ul>
                 </li>
